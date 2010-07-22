efd85ed84813c88da26d8f414ed15df6e0fd07eb
Derek Yang <yanghada@gmail.com>
2010-07-22 17:24:00 +0800

    Use pattern matching in OCUnitReportConverter.parse to simplify matching code

--- OCUnitReportConverter.groovy
+++ OCUnitReportConverter.groovy
@@ -1,3 +1,5 @@
+import java.util.regex.Matcher
+
 class OCUnitReportConverter {
     final String LINE_BREAK = "\r\n"
 
@@ -6,20 +8,21 @@ class OCUnitReportConverter {
         def testSuites = []
         def expSuite = /^Test Suite '([^']+)' started.*/
         def expFailureCount = /^Executed [\d]+ tests, with ([\d]+) failure.*/
+        
         ocunitOutput.split(LINE_BREAK).each {
-            def matcher = (it =~ expSuite)
-            if (matcher.matches())
-                testSuites << new TestSuite(name: matcher[0][1])
-            else {
-                matcher = (it =~ expFailureCount)
-                if (matcher.matches()) {                
+            switch (it) {
+                case ~expSuite:
+                    testSuites << new TestSuite(name: Matcher.getLastMatcher()[0][1])
+                    break
+                case ~expFailureCount:
                     // Use -1 as the flag to skip the very last failure count (which 
                     // we don't need)
                     if (testSuites[-1].numberOfFailures == -1)
-                        testSuites[-1].numberOfFailures = matcher[0][1] as int
-                }
-            }
+                        testSuites[-1].numberOfFailures = Matcher.getLastMatcher()[0][1] as int
+                    break                
+            }        
         }
+        
         // remove the first element since we don't need that one
         testSuites.remove(0)
         new JUnitReport(testSuites)
